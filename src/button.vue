06a45f4ef640c811c5c27eb26512fa31ddb94f3d
liuleill <18362960229@163.com>
2022-03-04 21:53:20 +0800

    安装parcel，讲button组件改为单文件组件

--- src/button.vue
+++ src/button.vue
@@ -1,29 +1,25 @@
 <template>
   <button class="g-button">按钮</button>
-
 </template>
 <script>
-export default {
-
-}
+  export default {}
 </script>
-<style lang="sass">
-.g-button{
-  height: var(--button-height);
-  padding:0 1em;
-  font:inherit;
-  border-radius: var(--border-radius);
-  background:var(--button-bg);
-  font-size:var(--font-size);
-  &:hover{
-    border-color:var(--border-color-hover);
+<style lang="scss">
+  .g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+    &:focus {
+      outline: none;
+    }
   }
-  &:active{
-    border-color:var(--button-active-bg);
-  }
-  &:focus{
-    outline:none;
-  }
-}
-
 </style>
